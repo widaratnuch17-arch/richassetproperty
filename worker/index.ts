06c39d18b056a9f1a7d039b3e820f1137e5486fd
widaratnuch17-arch <widaratnuch17@gmail.com>
2026-07-29 16:25:38 +0700

    Redirect local image transforms to source assets

--- worker/index.ts
+++ worker/index.ts
@@ -30,6 +30,11 @@ const worker = {
     const url = new URL(request.url);
 
     if (url.pathname === "/_vinext/image") {
+      const source = url.searchParams.get("url");
+      if (source?.startsWith("/") && !source.startsWith("//")) {
+        return Response.redirect(new URL(source, request.url), 302);
+      }
+
       const allowedWidths = [...DEFAULT_DEVICE_SIZES, ...DEFAULT_IMAGE_SIZES];
       return handleImageOptimization(request, {
         fetchAsset: (path) => env.ASSETS.fetch(new Request(new URL(path, request.url))),
